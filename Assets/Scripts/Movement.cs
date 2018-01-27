d8ff910bc94c0377e8eeeee3cfbc4fc6d7eabd67
JS44 <jacobsme@umich.edu>
2018-01-26 21:22:55 -0500

    Player prefab light movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    //rigidbody for character physics
+    Rigidbody rb;
+
+    //character's final run speed
+    public float maxSpeed = 1;
+
+    //speed which with character gets up to top speed
+    public float acceleration = 5;
+
+    //vertical speed character will jump with
+    public float jumpPower = 5;
+
+    Vector3 velocity;
+    Vector3 direction;
+
+    void Awake()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        direction = direction * maxSpeed - velocity;
+
+        velocity += direction;
+
+        rb.velocity = velocity;
+    }
+
+    public void SetDirection(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Movement mover;
+
+    void Awake()
+    {
+        mover = this.GetComponent<Movement>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0,0);
+
+        mover.SetDirection(direction);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+        }
+    }
+}
