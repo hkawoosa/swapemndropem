a82cc38d9e62823cf62dbcdda91efdc6df54af86
JS44 <jacobsme@umich.edu>
2018-01-27 17:57:59 -0500

    Added bullet delay and force delay

--- Assets/PlayerPush.cs
+++ Assets/PlayerPush.cs
@@ -8,14 +8,19 @@ public class PlayerPush : MonoBehaviour {
     public float distanceFromPlayer = 3f;
 
     public float force = 12;
+
+    public float forcePushDelay = 2;
+    float currentDelay = 0;
+
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown(this.tag + "Push"))
+        if (Input.GetButtonDown(this.tag + "Push") && currentDelay <=0)
         {
+            currentDelay = forcePushDelay;
             GameObject b;
             int dir = this.GetComponent<Direction>().GetDirection();
             if(dir == -1)
@@ -31,5 +36,6 @@ public class PlayerPush : MonoBehaviour {
             b.GetComponent<PushInfo>().setForce(force);
             b.GetComponent<Rigidbody>().velocity = dir * Vector3.right * force;
         }
+        currentDelay -= Time.deltaTime;
     }
 }
--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -8,21 +8,27 @@ public class PlayerGun : MonoBehaviour {
 
     public GameObject bullet;
 
-    public float bulletSpeed = 3f;
+    public float bulletSpeed = 6f;
 
-	// Use this for initialization
-	void Start () {
+    public float BulletDelay = .5f;
+    float currentDelay = 0;
+
+    // Use this for initialization
+    void Start () {
         dir = GetComponent<Direction>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown(this.tag + "Swap"))
+		if (Input.GetButtonDown(this.tag + "Swap") && currentDelay <= 0)
         {
+            currentDelay = BulletDelay;
             GameObject b = Instantiate(bullet, transform.position + (Vector3.right * dir.GetDirection()), Quaternion.identity);
             b.tag = this.tag;
             b.GetComponent<BulletOrigin>().SetPlayer(this.gameObject);
             b.GetComponent<Rigidbody>().velocity = dir.GetDirection() * Vector3.right * bulletSpeed;
         }
-	}
+
+        currentDelay -= Time.deltaTime;
+    }
 }
