d930763967771e0a8b544fb9c4a3e7c094de51e3
JS44 <jacobsme@umich.edu>
2018-01-27 16:03:41 -0500

    Added stun

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -29,6 +29,8 @@ public class Movement : MonoBehaviour {
 
     int jumpsRemaining = 2;
     public float jumpBuffer = 0f;
+    float stunnedFor = 0;
+    public float stunLength = 1f;
     Vector3 direction;
 
     bool stunned = false, usingHook = false;
@@ -44,9 +46,9 @@ public class Movement : MonoBehaviour {
     void FixedUpdate()
     {
         RaycastHit hit;
-
-       
-        /**if(Physics.SphereCast(this.transform.position, 1f, Vector3.down, out hit, cc.bounds.extents.x, MovingPlatform))
+        if(stunnedFor <= 0)
+        {
+            /**if(Physics.SphereCast(this.transform.position, 1f, Vector3.down, out hit, cc.bounds.extents.x, MovingPlatform))
         {
             Vector3 onPlatform = rb.velocity;
             onPlatform.x = hit.transform.position.x;
@@ -61,89 +63,75 @@ public class Movement : MonoBehaviour {
             }
             rb.velocity = onPlatform;
         }*/
-        if(!pd.IsDead())
-        {
-            Vector3 newX = rb.velocity;
-            newX.x = Input.GetAxis(this.tag + "Horizontal") * maxSpeed;
-            if (newX.x >= 0)
+            if (!pd.IsDead())
             {
-                newX.x = Mathf.Min(newX.x, maxSpeed);
+                Vector3 newX = rb.velocity;
+                newX.x = Input.GetAxis(this.tag + "Horizontal") * maxSpeed;
+                if (newX.x >= 0)
+                {
+                    newX.x = Mathf.Min(newX.x, maxSpeed);
+                }
+                else
+                {
+                    newX.x = Mathf.Max(newX.x, maxSpeed * -1);
+                }
+                rb.velocity = newX;
+
+                direction = new Vector3(Input.GetAxis(this.tag + "AimX"), Input.GetAxis(this.tag + "AimY"), 0);
+            }
+
+            Vector3 newY = rb.velocity;
+            if (Physics.SphereCast(this.transform.position, 1f, Vector3.down, out hit, cc.bounds.extents.x))
+            {
+                if (jumpBuffer <= 0)
+                {
+                    jumpsRemaining = 2;
+                }
+                else
+                {
+                    jumpBuffer -= Time.deltaTime;
+                }
+
             }
             else
             {
-                newX.x = Mathf.Max(newX.x, maxSpeed * -1);
+                if (jumpsRemaining == 2)
+                {
+                    jumpsRemaining = 1;
+                }
+                newY.y -= gravity * Time.deltaTime;
             }
-            rb.velocity = newX;
 
-            direction = new Vector3(Input.GetAxis(this.tag + "AimX"), Input.GetAxis(this.tag + "AimY"), 0);
-        }       
+            if (Input.GetButtonDown(this.tag + "Jump"))
+            {
+                if (jumpsRemaining == 2)
+                {
+                    newY.y = groundedJumpPower;
+                    jumpsRemaining--;
+                    jumpBuffer = .2f;
+                }
+                else if (jumpsRemaining == 1)
+                {
+                    newY.y = doubleJumpPower;
+                    jumpsRemaining--;
+                }
 
-        Vector3 newY = rb.velocity;
-        if (Physics.SphereCast(this.transform.position, 1f, Vector3.down, out hit, cc.bounds.extents.x))
-        {
-            if(jumpBuffer <= 0)
-            {
-                jumpsRemaining = 2;
             }
-            else
+            rb.velocity = newY;
+
+            if (Input.GetButtonDown(this.tag + "Wavedash"))
             {
-                jumpBuffer -= Time.deltaTime;
+                Vector3 newDirection = rb.velocity;
+                newDirection.x = Input.GetAxis(this.tag + "Horizontal") * maxSpeed;
+                newDirection.y = Input.GetAxis(this.tag + "Vertical") * maxSpeed;
             }
-            
         }
         else
         {
-            if(jumpsRemaining == 2)
-            {
-                jumpsRemaining = 1;
-            }
-            newY.y -= gravity * Time.deltaTime;
+            stunnedFor -= Time.deltaTime;
         }
-
-        if (Input.GetButtonDown(this.tag + "Jump"))
-        {
-            if(jumpsRemaining == 2)
-            {
-                newY.y = groundedJumpPower;
-                jumpsRemaining--;
-                jumpBuffer = .2f;
-            }
-            else if(jumpsRemaining == 1)
-            {
-                newY.y = doubleJumpPower;
-                jumpsRemaining--;
-            }
-            
-        }
-        rb.velocity = newY;
-
-        /*if(Input.GetButtonDown(this.tag + "Swap"))
-        {
-            /*instantiate(swapBullet, this.transform.postion + 1 in whatever x direction you are facing, this.transform.rotation)
-             * 
-             * 
-             * 
-             * 
-             
-
-
-            //this works for now but isnt what i want
-            if (this.tag == "P1_")
-            {
-                tag = "P2_";
-            }
-            else if (this.tag == "P2_")
-            {
-                tag = "P1_";
-            }
-        }*/
-
-        if(Input.GetButtonDown(this.tag + "Wavedash"))
-        {
-            Vector3 newDirection = rb.velocity;
-            newDirection.x = Input.GetAxis(this.tag + "Horizontal") * maxSpeed;
-            newDirection.y = Input.GetAxis(this.tag + "Vertical") * maxSpeed;
-        }
+       
+        
 
     }
 
@@ -170,6 +158,7 @@ public class Movement : MonoBehaviour {
     {
         string temp = this.tag;
         this.tag = other.tag;
+        stunnedFor = stunLength;
         other.tag = temp;
     }
 }
