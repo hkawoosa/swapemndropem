3b3071d0b5d181dfa65507408b15e65605065f29
JS44 <jacobsme@umich.edu>
2018-01-26 22:20:10 -0500

    gravity stuff

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,8 +7,10 @@ public class Movement : MonoBehaviour {
     //rigidbody for character physics
     Rigidbody rb;
 
+    CapsuleCollider cc;
+
     //character's final run speed
-    public float maxSpeed = 1;
+    public float maxSpeed = 5;
 
     //speed which with character gets up to top speed
     public float acceleration = 5;
@@ -16,19 +18,45 @@ public class Movement : MonoBehaviour {
     //vertical speed character will jump with
     public float jumpPower = 5;
 
+    public float gravity = 5;
+
     Vector3 velocity;
     Vector3 direction;
+
+    bool jump;
 
     void Awake()
     {
         rb = this.GetComponent<Rigidbody>();
+        cc = this.GetComponent<CapsuleCollider>();
     }
 
     void FixedUpdate()
     {
-        direction = direction * maxSpeed - velocity;
 
-        velocity += direction;
+        Vector3 xVelocity = velocity;
+        xVelocity.y = 0;
+        Vector3 groundDirection = direction * maxSpeed - xVelocity;
+
+        float stepSize = Mathf.Min(acceleration * Time.deltaTime, groundDirection.magnitude);
+        velocity += groundDirection * stepSize;
+
+        RaycastHit hit;
+
+        if (Physics.SphereCast(this.transform.position, 1f, Vector3.down, out hit, cc.bounds.max.x))
+        {
+           velocity.y = 0;
+        }
+        //else
+       // {
+        //    velocity.y -= gravity * Time.deltaTime;
+        //}
+
+        if (jump)
+        {
+            velocity.y = jumpPower;
+            jump = false;
+        }
 
         rb.velocity = velocity;
     }
@@ -36,5 +64,10 @@ public class Movement : MonoBehaviour {
     public void SetDirection(Vector3 direction)
     {
         this.direction = direction;
+    }
+
+    public void Jump()
+    {
+        jump = true;
     }
 }
