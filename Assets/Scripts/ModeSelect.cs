7d6c4ca9e569f45961ba6ded054de647eb960570
Alex Postmaa <apostmaa@umich.edu>
2018-01-28 13:59:01 -0500

    Menu Stuuf

---
+++ Assets/Scripts/ModeSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+
+public class ModeSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("P1_Jump"))
+        {
+            SceneManager.LoadScene("Map Select 1");
+        }
+        if (Input.GetButtonDown("P1_Push"))
+        {
+            SceneManager.LoadScene("Map Select 2");
+        }
+    }
+}
