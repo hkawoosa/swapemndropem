4f9867d6fbfa66821ac281dddfa4e6ade85a7eba
Alex Postmaa <apostmaa@umich.edu>
2018-01-28 14:28:33 -0500

    Menu System

---
+++ Assets/Scripts/MapSelect1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class MapSelect1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("P1_Jump"))
+        {
+            SceneManager.LoadScene("Not FD");
+        }
+        if (Input.GetButtonDown("P1_Push"))
+        {
+            SceneManager.LoadScene("Map 4");
+        }
+        if (Input.GetButtonDown("P1_Swap"))
+        {
+            SceneManager.LoadScene("Not Battlefield");
+        }
+    }
+}
---
+++ Assets/Scripts/MapSelect2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class MapSelect2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("P1_Jump"))
+        {
+            SceneManager.LoadScene("Not FD Two Player");
+        }
+        if (Input.GetButtonDown("P1_Push"))
+        {
+            SceneManager.LoadScene("Map 4 Two Player");
+        }
+        if (Input.GetButtonDown("P1_Swap"))
+        {
+            SceneManager.LoadScene("Not Battlefield 2 Player");
+        }
+
+    }
+}
